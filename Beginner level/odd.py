0e77361bca9b44ca7f248b691e9369592decb13f
vidhya002 <37336805+vidhya002@users.noreply.github.com>
2018-03-18 15:28:42 +0530

    Update odd.py

--- Beginner level/odd.py
+++ Beginner level/odd.py
@@ -1,9 +1,5 @@
-N=int(input())
-if(N>=1 and N<=100000):
-    if(N%2==0):
-        print("Even")
-    else:
-        print("Odd")
-elif(N=0):
-     print("Even")
-        
+N=int(raw_input())
+if N%2!=0:
+    print "Odd"
+else:
+    print "Even"
