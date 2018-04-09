0749badbdeef3ba1b01918e1c0acbd2c221fa9e9
vidhya002 <37336805+vidhya002@users.noreply.github.com>
2018-04-09 19:01:29 +0530

    Update prime intervals.py

--- Beginner level/prime intervals.py
+++ Beginner level/prime intervals.py
@@ -1,5 +1,8 @@
 c,d=input().split(" ")
-e=int(c,d)
-for i in range(2,e):
-    if(e%i=0):
-        print(e.end=' ')
+for i in range(int(c),int(d)):
+    if(i>1):
+        for n in range(2,i):
+            if(i%n==0):
+                break
+        else:
+            print(i,end=' ')
