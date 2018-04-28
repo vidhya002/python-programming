f9f50bde1f5b571efe54c2f012d4aec8f47ec984
vidhya002 <37336805+vidhya002@users.noreply.github.com>
2018-04-28 20:52:19 +0530

    Create right shift.py

---
+++ player/right shift.py
@@ -0,0 +1,15 @@
+d,s=input().split()
+d=int(d)
+s=int(s)
+g=s
+b=[int(input())for i in range(d)]
+if(d>s);
+    while(s>0):
+        f=b[-s]
+        print(f,end=' ')
+        s=s-1
+    c=d-g
+    for u in range(1,c):
+        print(b[u],end=' ')
+else:
+    print(b[i])       
