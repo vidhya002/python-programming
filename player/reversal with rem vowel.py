f08b5a216d0e61badfe04583d194735b261574f0
vidhya002 <37336805+vidhya002@users.noreply.github.com>
2018-05-01 12:39:24 +0530

    Update reversal with rem vowel.py

--- player/reversal with rem vowel.py
+++ player/reversal with rem vowel.py
@@ -1,10 +1,20 @@
-b=str(input()))
+b=str(input())
 b=b.lower()
-c=('a','e','i','o','u')
-for i in b:
-    if i in c:
-        b=b.replace(i,"")
-        d=b[::-1]
-        print(d)
-if i not in c:
-    print(b[::-1])
+g=length(b)
+h=[]
+for i in range(1,g):
+    h.append(b[-i])
+h.append(b[0])    
+for i in h:
+    if i=='a':
+        h.replace(i,"")
+    elif i=='e':
+        h.replace(i,"")
+    elif i=='i':
+        h.replace(i,"")
+    elif i=='o':
+        h.replace(i,"")
+    elif i=='u':
+        h.replace(i,"")
+k="".join(h)
+print(k)   
